84de66189030b52d86c47c97c98f4faeb78f4bca
PNNutkung <pipatpol.t@ku.th>
2016-06-05 17:39:38 +0700

    finish login page

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -2,13 +2,13 @@
     <div class="row">
         <?php if(validation_errors()) : ?>
             <div class="alert alert-danger" role="alert">
-                <?php validation_errors() ?>
+                <?php echo validation_errors(); ?>
             </div>
         <?php endif; ?>
         <?php if (isset($error)) : ?>
             <div class="col-md-12">
                 <div class="alert alert-danger" role="alert">
-                    <?php $error ?>
+                    <?php echo $error; ?>
                 </div>
             </div>
         <?php endif; ?>
@@ -16,7 +16,18 @@
             <div class="page-header">
                 <h1>Login</h1>
             </div>
-            <?php form_open() ?>
+            <?php echo form_open('/user/login'); ?>
+                <div class="form-group">
+                    <label for="username"></label>
+                    <input type="text" name="username" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label for="password"></label>
+                    <input type="password" name="password" placeholder="Password"></div>
+                <div class="form-group">
+                    <input type="submit" value="Login">
+                </div>
+            </form>
         </div>
     </div>
 </div>
