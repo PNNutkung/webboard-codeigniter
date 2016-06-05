398867dcd9d9a195e408b67a8e290aead9200286
Pipatpol Tanavongchinda <Pipatpol Tanavongchinda>
2016-06-05 10:30:28 +0700

    Finish SQL starter file

--- database_starter_file/create_this.sql
+++ database_starter_file/create_this.sql
@@ -14,7 +14,23 @@ CREATE TABLE thread(
     threadID INT NOT NULL AUTO_INCREMENT,
     threadName VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
     threadDeatil LONGTEXT COLLATE utf8_unicode_ci NOT NULL,
-    createDate DATETIME NOTNULL DEFAULT NOW(),
+    createDate DATETIME NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`threadID`),
     FOREIGN KEY (`ownerID`) REFERENCES user(`userID`)
+);
+
+DROP TABLE IF EXIST `comment`;
+CREATE TABLE comment(
+    commentID INT NOT NULL AUTO_INCREMENT,
+    commentDetail LONGTEXT COLLATE utf8_unicode_ci NOT NULL,
+    createDate DATETIME NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (`commentID`),
+    FOREIGN KEY (`ownerID`) REFERENCES user(`userID`)
+);
+
+CREATE TABLE board(
+    boardID INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`boardID`),
+    FOREIGN KEY (`threadID`) REFERENCES thread(`threadID`),
+    FOREIGN KEY (`comment`) REFERENCES comment(`commentID`)
 );
