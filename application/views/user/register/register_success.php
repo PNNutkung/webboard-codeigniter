8201250443587c54c1ff9f6f1cfccc7c1ba44e94
PNNutkung <pipatpol.t@ku.th>
2016-06-05 18:06:07 +0700

    Complete register success pae

---
+++ application/views/user/register/register_success.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="col-me-12">
+            <div class="page-header">
+                <h1>Thank you for registering your new account!</h1>
+            </div>
+            <p>You have successfully register.</p>
+        </div>
+    </div>
+</div>
